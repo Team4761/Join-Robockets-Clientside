1a92e4500d416dd96da06b484ec0d0b3bc7b3373
Simon Andrews <simon.andrews212@gmail.com>
2016-05-07 13:59:25 -0400

    Window now shows HSV-filtered image, and has tuning sliders.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ using namespace cv;
 using namespace std;
 
 string window_title = "Join-Robockets Clientside Software";
+int h_lower = 0, s_lower = 0, v_lower = 0;
+int h_upper = 180, s_upper = 255, v_upper = 255;
 
 int main() {
     //Initialize camera.
@@ -16,12 +18,21 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    Mat frame;
+    Mat frame, hsv, filtered;
     namedWindow(window_title);
+    createTrackbar("Hue Lower", window_title, &h_lower, 180, NULL);
+    createTrackbar("Hue Upper", window_title, &h_upper, 180, NULL);
+    createTrackbar("Saturation Lower", window_title, &s_lower, 255, NULL);
+    createTrackbar("Saturation Upper", window_title, &s_upper, 255, NULL);
+    createTrackbar("Value Lower", window_title, &v_lower, 255, NULL);
+    createTrackbar("Value Upper", window_title, &v_upper, 255, NULL);
+
     while(1) {
         //Capture and show image.
         cap >> frame;
-        imshow(window_title, frame);
+        cvtColor(frame, hsv, CV_BGR2HSV);
+        inRange(hsv, Scalar(h_lower, s_lower, v_lower), Scalar(h_upper, s_upper, v_upper), filtered);
+        imshow(window_title, filtered);
 
         //Quit if any key has been pressed.
         if(waitKey(30) >= 0) {
