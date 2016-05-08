813b038c7a7b34567a322d226541dca82c1a645c
Simon Andrews <simon.andrews212@gmail.com>
2016-05-08 13:47:42 -0400

    Add callback to make it so that new Scalar instances aren't created every time through the loop, and add an exposure slider.
    Exposure slider hasn't been tested since my laptop webcam does not support this feature, but everything else about it seems to be working fine. We will need to try this out with one of the Lifecams.

--- main.cpp
+++ main.cpp
@@ -9,32 +9,42 @@ using namespace std;
 string window_title = "Join-Robockets Clientside Software";
 int h_lower = 0, s_lower = 0, v_lower = 0;
 int h_upper = 180, s_upper = 255, v_upper = 255;
+Scalar hsv_lower(h_lower, s_lower, v_lower);
+Scalar hsv_upper(h_upper, s_upper, v_upper);
+
+int exposure = 50;
+VideoCapture cap(CV_CAP_ANY);
+
+void exposure_callback(int, void*) {
+    cap.set(CV_CAP_PROP_EXPOSURE, exposure);
+}
+
+void filter_callback(int, void*) {
+    hsv_lower = Scalar(h_lower, s_lower, v_lower);
+    hsv_upper = Scalar(h_upper, s_upper, v_upper);
+}
 
 int main() {
-    //Initialize camera.
-    VideoCapture cap(CV_CAP_ANY);
+    //Check if camera works
     if(!cap.isOpened()) {
         cerr << "Could not open image capture device!" << endl;
         exit(EXIT_FAILURE);
     }
 
     Mat frame, hsv, filtered;
-    Scalar hsv_lower;
-    Scalar hsv_upper;
     namedWindow(window_title);
-    createTrackbar("H Lower", window_title, &h_lower, 180);
-    createTrackbar("H Upper", window_title, &h_upper, 180);
-    createTrackbar("S Lower", window_title, &s_lower, 255);
-    createTrackbar("S Upper", window_title, &s_upper, 255);
-    createTrackbar("V Lower", window_title, &v_lower, 255);
-    createTrackbar("V Upper", window_title, &v_upper, 255);
+    createTrackbar("H Lower", window_title, &h_lower, 180, filter_callback);
+    createTrackbar("H Upper", window_title, &h_upper, 180, filter_callback);
+    createTrackbar("S Lower", window_title, &s_lower, 255, filter_callback);
+    createTrackbar("S Upper", window_title, &s_upper, 255, filter_callback);
+    createTrackbar("V Lower", window_title, &v_lower, 255, filter_callback);
+    createTrackbar("V Upper", window_title, &v_upper, 255, filter_callback);
+    createTrackbar("Exposure", window_title, &exposure, 255, exposure_callback);
 
     while(1) {
         //Capture and show image.
         cap >> frame;
         cvtColor(frame, hsv, CV_BGR2HSV);
-        hsv_lower = Scalar(h_lower, s_lower, v_lower);
-        hsv_upper = Scalar(h_upper, s_upper, v_upper);
         inRange(hsv, hsv_lower, hsv_upper, filtered);
         imshow(window_title, filtered);
 
